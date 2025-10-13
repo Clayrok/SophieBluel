a5a40ce195d8e8c97fb633fa64218254dd15bc03
CLAY\Clay <contact.chaumin@gmail.com>
2025-10-14 00:32:46 +0200

    works loaded and categories generated in one API call

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,16 +1,26 @@
+let categories = [];
+
 document.addEventListener("DOMContentLoaded", function (e) {
-    loadWorks();
+    init();
 });
 
-async function loadWorks() {
-    fetch("http://localhost:5678/api/works").then(async (response) => {
+function init() {
+    loadWorks();
+}
+
+ function loadWorks() {
+    fetch("http://localhost:5678/api/works").then((response) => {
         if (response) {
-            response.json().then(async (works) => {
+            response.json().then((works) => {
                 const gallery = document.querySelector(".gallery");
                 gallery.innerHTML = '';
 
+                resetCategories();
+
                 works.forEach(work => {
                     const newWork = document.createElement("figure");
+                    newWork.classList.add("work");
+                    newWork.dataset.categoryId = work.category.id;
 
                     const img = document.createElement("img");
                     img.src = work.imageUrl;
@@ -22,8 +32,63 @@ async function loadWorks() {
                     newWork.appendChild(caption);
 
                     gallery.appendChild(newWork);
+
+                    addCategory(work.category);
                 });
             });
+        }
+    });
+}
+
+function resetCategories() {
+    const filters = document.querySelector("#filters");
+
+    categories = [];
+    filters.innerHTML = "";
+
+    addCategory({ id: 0, name: "Tous" }, true);
+}
+
+function addCategory(category, active = false) {
+    const filters = document.querySelector("#filters");
+
+    if (!categories.some(el => el.id === category.id && el.name === category.name)) {
+        categories.push(category);
+
+        const newBtn = document.createElement("button");
+        newBtn.innerText = category.name;
+        newBtn.classList.add("filter-button");
+        if (active) newBtn.classList.add("active");
+        newBtn.dataset.filterId = category.id;
+
+        newBtn.addEventListener("click", function (e) {
+            setFilter(this.dataset.filterId);
+        });
+
+        filters.appendChild(newBtn);
+    }
+}
+
+function setFilter(filterId) {
+    const filterButtons = document.querySelectorAll(".filter-button");
+    filterButtons.forEach(button => {
+        button.classList.remove("active");
+    });
+
+    document.querySelector(`.filter-button[data-filter-id="${filterId}"]`).classList.add("active");
+
+    const workTiles = document.querySelectorAll(".work");
+    workTiles.forEach(tile => {
+        if (filterId == 0) {
+            tile.classList.remove("hidden");
+        }
+        else {
+            if (tile.dataset.categoryId == filterId) {
+                tile.classList.remove("hidden");
+            }
+            else {
+                tile.classList.add("hidden");
+            }
         }
     });
 }
